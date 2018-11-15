d95e0170744e7e51462591be054fa23e2cba3961
TrungMinhIT <1651012207trung@ou.edu.vn>
2018-11-15 22:20:22 +0700

    Testcase them nullCV

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -155,6 +155,15 @@ namespace UnitTestProject1
             cn.Them(daTt, "123", "Nguyen Van A", "31 NK", " ", "Nhan Vien");
             Assert.AreEqual(1, daTt.Rows.Count);
         }
+        [TestMethod]
+        public void TestThemNullChucVu()
+        {
+            SetUp();
+            DataTable daTt = ds.Tables[0];
+            Assert.AreEqual(0, daTt.Rows.Count);
+            cn.Them(daTt, "123", "Nguyen Van A", "31 NK", "0123465789", " ");
+            Assert.AreEqual(1, daTt.Rows.Count);
+        }
     }
 }
 
