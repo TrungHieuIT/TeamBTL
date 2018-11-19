fd88b27bd52c21c6d9398d698cdbed6dd3c96cc9
TrungMinhIT <1651012207trung@ou.edu.vn>
2018-11-19 11:23:50 +0700

    TestUPDATE

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -309,7 +309,22 @@ namespace UnitTestProject1
             cn.Del(0, daTx);
             cn.Del(0, daTx);
         }
-
+        [TestMethod]
+        public void TestUpdate()
+        {
+            SetUp();
+            DataTable daTa = ds.Tables[0];
+            cn.Them(daTa, "12332", "Nguyen Van A", "acb", "0123", "Nhân Viên");
+            cn.Them(daTa, "123432", "Nguyen Van B", "acb", "0123", "Nhân Viên");
+            cn.Them(daTa, "123532", "Nguyen Van C", "acb", "0123", "Nhân Viên");
+            cn.Update(daTa);
+            DataTable tbNew = ds.Tables[0];
+            Assert.AreEqual(3, tbNew.Rows.Count);
+            cn.Del(0, daTa);
+            cn.Del(0, daTa);
+            cn.Del(0, daTa);
+        }
+       
     }
 }
 
