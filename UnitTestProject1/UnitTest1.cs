0e80300cfc088dd849cb3c4f2f778a0015860570
HoangDuy10021998 <1651010032duy@ou.edu.vn>
2018-11-19 16:07:06 +0700

    UnitTest Update

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -169,6 +169,16 @@ namespace UnitTestProject1
             Assert.AreEqual(1, daTt.Rows.Count);
         }
         [TestMethod]
+        public void TestThemTRUNG()
+        {
+            SetUp();
+            DataTable daTt = ds.Tables[0];//KHOI TAO
+            cn.Them(daTt, "123", "Nguyen Van A", "31 NK", "0123465789", "Nhan Vien");//THEM VI TRI 0
+            cn.Them(daTt, "123", "Nguyen Van A", "31 NK", "0123465789", "Nhan Vien");//THEM VI TRI 1 TRUNG 0
+            Assert.AreEqual(1, daTt.Rows.Count);// 1 DUNG 2 SAI
+        }
+
+        [TestMethod]
         public void TestSuaID()
         {
             SetUp();
@@ -224,123 +234,7 @@ namespace UnitTestProject1
             cn.Sua(daTs.Rows[0], gd);
             Assert.AreEqual("Giam Doc", daTs.Rows[0][4]);
         }
-
-        [TestMethod]
-        public void TestXoa()
-        {
-            SetUp();
-            DataTable daTx = ds.Tables[0];
-            cn.Them(daTx, "1232123", "Nguyen Van A", "acb", "0123", "Nhân Viên");
-            cn.Them(daTx, "12343", "Nguyen Van B", "acb", "0123", "Nhân Viên");
-            cn.Them(daTx, "12353", "Nguyen Van C", "acb", "0123", "Nhân Viên");
-
-            cn.Del(1, daTx);
-            Assert.AreEqual(2, daTx.Rows.Count);
-            cn.Del(0, daTx);
-            cn.Del(0, daTx);
-        }
-        [TestMethod]
-        public void TestXoaID()
-        {
-            SetUp();
-            DataTable daTx = ds.Tables[0];
-            cn.Them(daTx, "1232123", "Nguyen Van A", "acb", "0123", "Nhân Viên");
-            cn.Them(daTx, " ", "Nguyen Van B", "acb", "0123", "Nhân Viên");
-            cn.Them(daTx, "12353", "Nguyen Van C", "acb", "0123", "Nhân Viên");
-
-            cn.Del(1, daTx);
-            Assert.AreEqual(2, daTx.Rows.Count);
-            cn.Del(0, daTx);
-            cn.Del(0, daTx);
-        }
-        [TestMethod]
-        public void TestXoaHoTen()
-        {
-            SetUp();
-            DataTable daTx = ds.Tables[0];
-            cn.Them(daTx, "1232123", "Nguyen Van A", "acb", "0123", "Nhân Viên");
-            cn.Them(daTx, "12343", " ", "acb", "0123", "Nhân Viên");
-            cn.Them(daTx, "12353", "Nguyen Van C", "acb", "0123", "Nhân Viên");
-
-            cn.Del(1, daTx);
-            Assert.AreEqual(2, daTx.Rows.Count);
-            cn.Del(0, daTx);
-            cn.Del(0, daTx);
-        }
-        [TestMethod]
-        public void TestXoaDiaChi()
-        {
-            SetUp();
-            DataTable daTx = ds.Tables[0];
-            cn.Them(daTx, "1232123", "Nguyen Van A", "acb", "0123", "Nhân Viên");
-            cn.Them(daTx, "12343", "Nguyen Van B ", " ", "0123", "Nhân Viên");
-            cn.Them(daTx, "12353", "Nguyen Van C", "acb", "0123", "Nhân Viên");
-
-            cn.Del(1, daTx);
-            Assert.AreEqual(2, daTx.Rows.Count);
-            cn.Del(0, daTx);
-            cn.Del(0, daTx);
-        }
-        [TestMethod]
-        public void TestXoaSDT()
-        {
-            SetUp();
-            DataTable daTx = ds.Tables[0];
-            cn.Them(daTx, "1232123", "Nguyen Van A", "acb", "0123", "Nhân Viên");
-            cn.Them(daTx, "12343", "Nguyen Van B ", "acb ", " ", "Nhân Viên");
-            cn.Them(daTx, "12353", "Nguyen Van C", "acb", "0123", "Nhân Viên");
-
-            cn.Del(1, daTx);
-            Assert.AreEqual(2, daTx.Rows.Count);
-            cn.Del(0, daTx);
-            cn.Del(0, daTx);
-        }
-        [TestMethod]
-        public void TestXoaChucVu()
-        {
-            SetUp();
-            DataTable daTx = ds.Tables[0];
-            cn.Them(daTx, "1232123", "Nguyen Van A", "acb", "0123", "Nhân Viên");
-            cn.Them(daTx, "12343", "Nguyen Van B ", "acb ", "0123", " ");
-            cn.Them(daTx, "12353", "Nguyen Van C", "acb", "0123", "Nhân Viên");
-
-            cn.Del(1, daTx);
-            Assert.AreEqual(2, daTx.Rows.Count);
-            cn.Del(0, daTx);
-            cn.Del(0, daTx);
-        }
-        [TestMethod]
-        public void TestUpdate()
-        {
-            SetUp();
-            DataTable daTa = ds.Tables[0];
-            cn.Them(daTa, "12332", "Nguyen Van A", "acb", "0123", "Nhân Viên");
-            cn.Them(daTa, "123432", "Nguyen Van B", "acb", "0123", "Nhân Viên");
-            cn.Them(daTa, "123532", "Nguyen Van C", "acb", "0123", "Nhân Viên");
-            cn.Update(daTa);
-            DataTable tbNew = ds.Tables[0];
-            Assert.AreEqual(3, tbNew.Rows.Count);
-            cn.Del(0, daTa);
-            cn.Del(0, daTa);
-            cn.Del(0, daTa);
-        }
-        [TestMethod]
-        public void TestUpdateID()
-        {
-            SetUp();
-            DataTable daTa = ds.Tables[0];
-            cn.Them(daTa, "12332", "Nguyen Van A", "acb", "0123", "Nhân Viên");
-            cn.Them(daTa, "1564", "Nguyen Van B", "acb", "0123", "Nhân Viên");
-            cn.Them(daTa, "123532", "Nguyen Van C", "acb", "0123", "Nhân Viên");
-            cn.Update(daTa);
-
-            DataTable tbNew = ds.Tables[0];
-            Assert.AreEqual(3, tbNew.Rows.Count);
-            cn.Del(0, daTa);
-            cn.Del(0, daTa);
-            cn.Del(0, daTa);
-
-        }
+        
     }
 }
 
